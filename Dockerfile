67048ca1b9adf664d4723b869c2a549722c5cdb1
Leonid Kolesnichenko <xperience439@gmail.com>
2025-01-28 15:07:34 +0300

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM python:3.12-slim
 
+RUN apt-get update
+RUN apt-get install -y libjemalloc-dev expat
+ENV LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so
+
 WORKDIR /app
 ADD . /app
 RUN pip install -e .
-
-RUN apt-get update
-RUN apt-get install -y libjemalloc-dev
-ENV LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so
+RUN pip install setuptools --upgrade
 
 CMD ["python", "worker.py"]
--- worker.py
+++ worker.py
@@ -1,10 +1,17 @@
+import os
+
 from rq import Worker
+from eoreader.reader import Reader
+from eoreader.bands import *
 
 from tiler.config import REDIS_CONNECTION
 
 
 def tile(s2_product_path, band, z, x, y, tilesize=256):
-    return s2_product_path, band, z, x, y, tilesize
+    s2_product_path_full_path = os.path.join('/opt/satellite-data', s2_product_path)
+    product = Reader().open(s2_product_path_full_path)
+    print(product, flush=True)
+    return product
 
 
 if __name__ == "__main__":
